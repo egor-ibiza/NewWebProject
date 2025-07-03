c49461d4b97e5b5e0763fcf55a2d0eae3973d198
Egor <egoribiza@mail.ru>
2025-07-03 15:18:06 +0200

    Create script.js
    Add script.js to handle heading click

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.addEventListener("click", () => {
+    alert("Заголовок был нажат!");
+  });
+});
